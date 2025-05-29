b0f29bafaf3f473aff4c344c777de530fc7aee60
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-29 08:57:12 +0200

    Create contact.blade.php

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('main')
+    <h1 class="text-3xl font-bold mb-4">{{ __('pages.contact.title') }}</h1>
+    <p>{{ __('pages.contact.instructions') }}</p>
+@endsection
