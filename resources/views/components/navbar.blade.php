227a59af53ceb9c41470065446953418ec93d86c
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-05-29 13:56:03 +0200

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -15,18 +15,22 @@
 
         {{-- Language Dropdown --}}
         <div class="relative inline-block text-left">
-            <button id="lang-button" aria-haspopup="true" aria-expanded="false" aria-label="Language menu" class="focus:outline-none">
+            <button id="lang-button" aria-haspopup="true" aria-expanded="false" aria-label="Language menu" class="flex items-center space-x-1 focus:outline-none">
+                <i data-lucide="globe" class="w-5 h-5 text-gray-700 dark:text-gray-300"></i>
                 <img src="{{ asset("img/SetLocale/$locale.png") }}" alt="{{ $locale }} flag" class="w-7 h-5 rounded">
             </button>
 
-            <div id="lang-menu" class="absolute right-0 mt-2 w-20 rounded shadow-lg bg-white dark:bg-gray-700 hidden z-50">
+            <div id="lang-menu" class="absolute right-0 mt-2 w-28 rounded shadow-lg bg-white dark:bg-gray-700 hidden z-50">
                 <ul>
                     @foreach ($langFolders as $lang)
                         <li>
                             <a href="{{ route('lang.set', ['locale' => $lang]) }}"
                                class="flex items-center px-4 py-2 w-full hover:bg-gray-200 dark:hover:bg-gray-600 {{ $locale === $lang ? 'bg-gray-200 dark:bg-gray-600 font-semibold' : '' }}">
                                 <img src="{{ asset("img/SetLocale/$lang.png") }}" alt="{{ $lang }} flag" class="w-7 h-5 mr-2 rounded">
-                                {{ strtoupper($lang) }}
+                                <span class="flex-grow">{{ strtoupper($lang) }}</span>
+                                @if ($locale === $lang)
+                                    <i data-lucide="check" class="w-4 h-4 text-green-500"></i>
+                                @endif
                             </a>
                         </li>
                     @endforeach
@@ -99,6 +103,7 @@
         const savedTheme = localStorage.getItem('theme') || 'auto';
         applyTheme(savedTheme);
         setIcon(savedTheme);
+        lucide.createIcons();
     });
 
     document.addEventListener('click', e => {
